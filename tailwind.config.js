b37233fd096eb831891a847467a8331566e6c348
Somesh Saraf <someshsaraf@somesh.saraf>
2025-11-11 10:29:47 +0530

    New change to use fixtures in start match

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default { content:['./index.html','./src/**/*.{js,jsx}'], theme:{extend:{}}, plugins:[], darkMode:'class' }
+export default { content:['./index.html','./src/**/*.{js,jsx}'], theme:{extend:{}}, plugins:[] }
