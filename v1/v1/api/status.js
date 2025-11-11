04cbe08de44c874e18f9d5ef3ec361eb5890f7e3
Somesh Saraf <someshsaraf@somesh.saraf>
2025-11-11 15:47:18 +0530

    New change to use fixtures in start match

--- v1/v1/api/status.js
+++
@@ -1,2 +0,0 @@
-import { getRedis } from './_kv.js'
-export default async function handler(req,res){res.setHeader('Access-Control-Allow-Origin','*');res.setHeader('Cache-Control','no-store');res.setHeader('Content-Type','application/json');res.status(200).json({kv:!!getRedis()})}
